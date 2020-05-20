2d69e7894f0b12abad44bc0ba4de76e09e4eae91
levanayang <levanavyang@gmail.com>
2020-05-20 14:32:44 -0700

    created new .env file for api keys

--- server/models/weather.js
+++ server/models/weather.js
@@ -1,12 +1,10 @@
 const request = require('request-promise');
-
-// for an api key to interact with weather api
-const API_KEY = '00ab3b20f10d5adc2e9e456e9a145210';
+require('dotenv').config();
 
 class Weather {
     static retrieveByCity(city, callback) {
         request({
-            uri: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`,
+            uri: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER_API_KEY}&units=metric`,
             json: true
         }).then((res) => {
             callback(res);
