4b3d44e1e1599451e8c1eb7c962c1a06165bff0d
levanayang <levanavyang@gmail.com>
2020-05-20 13:10:40 -0700

    website works locally but not when pushed to heroku

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const ENV = process.env.NODE_ENV;
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());
