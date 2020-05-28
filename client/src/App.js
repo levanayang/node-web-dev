632ccb5ab83429c2a42e41afecae89eef1228157
levanayang <levanavyang@gmail.com>
2020-05-28 14:01:11 -0700

    added changes new links to navbar menu

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 import {
   Container,
-  Navbar,
-  NavbarBrand,
-  Row,
-  Col,
   Jumbotron
   } from 'reactstrap';
 
 import Resume from './Resume';
+import Menu from './Menu'
 
 class App extends Component {
   constructor(props) {
@@ -25,17 +22,11 @@ class App extends Component {
   render() {
     return (
         <Container fluid className="main">
-            <Navbar dark color="dark">
-                <NavbarBrand href="/">Home</NavbarBrand>
-            </Navbar>
-            <Row>
-            <Col>
-              <Jumbotron>
+            <Menu/>
+            <Jumbotron>
                 <h1 className="display-3">Levana Yang</h1>
                 <p className="lead">Welcome to my site!</p>
-              </Jumbotron>
-            </Col>
-            </Row>
+            </Jumbotron>
             <Resume src={"https://docs.google.com/document/d/e/2PACX-1vRAatOYzlULG_9rKJ6R6Uj3ugYbT-2KVuNeqxfLNsCn40kns9roG22W63oLhGrX5Hrtd01IUQbrzJ0y/pub?embedded=true"}/>
         </Container>
     );
---
+++ client/src/Menu.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink
+} from 'reactstrap';
+
+class Menu extends React.Component{
+    constructor(props) {
+        super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false,
+            navCollapsed: false
+        }
+    }
+
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <Navbar dark
+                        color="dark"
+                        expand="md"
+                        fixed="top"
+                >
+                    <NavbarBrand href="/">Home</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle}/>
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="/about/">About Me</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="/projects/">Projects</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="/links/">Links</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+export default Menu
