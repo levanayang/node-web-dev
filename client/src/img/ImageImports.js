667b4d9f08e7174e5385aed7ec77af9c382e0d51
levanayang <levanavyang@gmail.com>
2020-06-24 18:28:26 -0700

    changed capitalization image path

--- client/src/img/ImageImports.js
+++ client/src/img/ImageImports.js
@@ -1,6 +1,6 @@
 import beeb_and_keyboard from './beeb_and_keyboard.jpg'
 import car_rental_er_diagram from './car_rental_er_diagram.jpg'
-import car_rental_preview from './car_rental_preview.jpg'
+import car_rental_preview from './car_rental_preview.JPG'
 import discribe_audio_profile from './discribe_audio_profile.JPG'
 import discribe_preview from './discribe_preview.JPG'
 import discribe_wiki_preview from './discribe_wiki_preview.JPG'
