f37ea76bc202658960e24777b82e1cf97f8f520f
levanayang <levanavyang@gmail.com>
2020-06-26 17:45:23 -0700

    removed /build from gitignore

---
+++ client/build/precache-manifest.f0d8342847864829b96d89b14a337318.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ded453bd53943f66a40de80915b08bcf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c25673bb35711920efee",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "178753c94f780ed40642",
+    "url": "/static/css/main.5783908d.chunk.css"
+  },
+  {
+    "revision": "c25673bb35711920efee",
+    "url": "/static/js/2.90c4f7ee.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.90c4f7ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "178753c94f780ed40642",
+    "url": "/static/js/main.6001fa7c.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  },
+  {
+    "revision": "208fb956cc3774edfd98d7235c8f3524",
+    "url": "/static/media/beeb_and_keyboard.68d87aa1.jpg"
+  },
+  {
+    "revision": "fe21bc1be56cbe6f86c5fee815540803",
+    "url": "/static/media/car_rental_er_diagram.5fac3e75.jpg"
+  },
+  {
+    "revision": "b520aebb0aaf7d90fbe9714ef5133662",
+    "url": "/static/media/car_rental_preview.b520aebb.JPG"
+  },
+  {
+    "revision": "ee0407e29305a9ea38bf6cf90c736354",
+    "url": "/static/media/discribe_audio_profile.ee0407e2.JPG"
+  },
+  {
+    "revision": "05a9b56ab966663d5b7f10d12d6066fb",
+    "url": "/static/media/discribe_preview.05a9b56a.JPG"
+  },
+  {
+    "revision": "511e67507b1958b50a7a94a71473217f",
+    "url": "/static/media/discribe_wiki_preview.511e6750.JPG"
+  },
+  {
+    "revision": "1c4e4c8ee213ceba52bb6bcac4a7dfc3",
+    "url": "/static/media/flat_ear_beeb.6e238153.jpg"
+  },
+  {
+    "revision": "7478bf5908a408701df34d5aabb43dde",
+    "url": "/static/media/inti_touch.471a4ce4.jpg"
+  },
+  {
+    "revision": "e651099bfcbb72a0e6fe3a5895dd9213",
+    "url": "/static/media/me_profile.37dcfc03.jpg"
+  },
+  {
+    "revision": "c75de8af8e31ce369996e20c3e3b0eef",
+    "url": "/static/media/me_with_cup_side.3b2e0986.jpg"
+  },
+  {
+    "revision": "ba76f584d94d7c8fdf3d56e0edf52cf4",
+    "url": "/static/media/portfolio_preview.ba76f584.JPG"
+  }
+]);
